b98975977125b5027db28fc1f9cda5bb7d9b4ea2
Tyler Leonhardt <tylerl0706@gmail.com>
2020-07-02 12:52:09 -0700

    additional script stuff

--- Dockerfile
+++ Dockerfile
@@ -3,12 +3,6 @@ SHELL ["pwsh", "-Command"]
 
 RUN Install-Module PowerShellForGitHub -Force -Scope AllUsers
 
-RUN if (!$env:GITHUB_TOKEN) { throw "Environment variable for GITHUB_TOKEN not found" } \
-    # Setup PowerShellForGitHub with GITHUB_TOKEN
-    $secureString = $env:GITHUB_TOKEN | ConvertTo-SecureString -AsPlainText -Force \
-    $cred = [System.Management.Automation.PSCredential]::new("username is ignored", $secureString) \
-    Set-GitHubAuthentication -Credential $cred
-
 COPY LICENSE README.md /
 
 COPY entrypoint.ps1 /entrypoint.ps1
--- entrypoint.ps1
+++ entrypoint.ps1
@@ -1,1 +1,14 @@
-Get-GitHubEmoji
+if (!$env:GITHUB_TOKEN) {
+    throw "Environment variable for GITHUB_TOKEN not found"
+}
+
+# Setup PowerShellForGitHub with GITHUB_TOKEN
+$secureString = $env:GITHUB_TOKEN | ConvertTo-SecureString -AsPlainText -Force
+$cred = [System.Management.Automation.PSCredential]::new("username is ignored", $secureString)
+Set-GitHubAuthentication -Credential $cred
+
+$env:GITHUB_REPOSITORY
+$repoArgs = $env:GITHUB_REPOSITORY -split '/'
+Set-GitHubConfiguration -DefaultOwnerName $repoArgs[0] -DefaultRepositoryName $repoArgs[1]
+
+Get-GitHubPullRequest
