57853520817851e9f8a4034edc96b4355632a01e
Tyler Leonhardt <tylerl0706@gmail.com>
2020-07-02 13:12:12 -0700

    simplify script

--- entrypoint.ps1
+++ entrypoint.ps1
@@ -2,13 +2,13 @@ if (!$env:GITHUB_TOKEN) {
     throw "Environment variable for GITHUB_TOKEN not found"
 }
 
-# Setup PowerShellForGitHub with GITHUB_TOKEN
+# Setup PowerShellForGitHub for use in GitHub Actions
 $secureString = $env:GITHUB_TOKEN | ConvertTo-SecureString -AsPlainText -Force
 $cred = [System.Management.Automation.PSCredential]::new("username is ignored", $secureString)
 Set-GitHubAuthentication -Credential $cred
-
-$env:GITHUB_REPOSITORY
 $repoArgs = $env:GITHUB_REPOSITORY -split '/'
 Set-GitHubConfiguration -DefaultOwnerName $repoArgs[0] -DefaultRepositoryName $repoArgs[1]
 
-Get-GitHubPullRequest
+# Put custom code after here
+
+Get-GitHubPullRequest | Select-Object title,number,http_url
